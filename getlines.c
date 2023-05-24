389aa890ce4de06083e751bec734fb4802cb6451
Your Name <you@example.com>
2023-05-23 20:03:52 -0700

    edited

--- getlines.c
+++ getlines.c
@@ -1,23 +1,23 @@
 #include "shell.h"
 
 /**
-* _getline - This function Reads The Inputs By User From Stdin
+* _getline - Read The Input By User From Stdin
 *
-* Return: Inputs that we submitted
+* Return: Input
 */
 char *_getline(void)
 {
-	int v = 0;
+	int i = 0;
 	ssize_t nread;
-	char d = 0, *buffer = malloc(sizeof(char) * BUFFER_SIZE);
+	char c = 0, *buffer = malloc(sizeof(char) * BUFFER_SIZE);
 
 	if (buffer == NULL)
 	{
 		return (NULL);
 	}
-	while (d != EOF && d != '\n')
+	while (c != EOF && c != '\n')
 	{
-		nread = read(STDIN_FILENO, &d, 1);
+		nread = read(STDIN_FILENO, &c, 1);
 		if (nread == 0 || nread == -1)
 		{
 			free(buffer);
@@ -26,40 +26,40 @@ char *_getline(void)
 			if (nread == -1)
 				perror("Error: ");
 		}
-		buffer[v] = d;
+		buffer[i] = c;
 		if (buffer[0] == '\n')
 		{
 			free(buffer);
 			return ("\0");
 		}
-		if (v + 1 >= BUFFER_SIZE)
+		if (i + 1 >= BUFFER_SIZE)
 		{
-			buffer = _realloc(buffer, v + 1);
+			buffer = _realloc(buffer, i + 1);
 			if (buffer == NULL)
 			{
 				free(buffer);
 				return (NULL);
 			}
 		}
-		v++;
+		i++;
 	}
-	buffer[v - 1] = '\0';
+	buffer[i - 1] = '\0';
 	hash_handler(buffer);
 	return (buffer);
 }
 
 /**
-* hash_handler - This function handles the hash character in input
-* @buff: input buffer that we implemented
+* hash_handler - handle hash character in input
+* @buff: input buffer
 */
 void hash_handler(char *buff)
 {
-	int v = 0;
+	int i = 0;
 
-	while (buff[v] != '\0')
+	while (buff[i] != '\0')
 	{
-		if (buff[v] == '#')
-			buff[v] = '\0';
-		v++;
+		if (buff[i] == '#')
+			buff[i] = '\0';
+		i++;
 	}
 }
